0367a01ca31c7bcdce934959ce5fe406d4b8bbc0
Krofah <amponsahkrofah@gmail.com>
2023-12-18 21:49:10 +0000

    task 0

--- converter.py
+++ converter.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 import argparse
-from datetime import datetime
-import requests
 from decimal import Decimal, ROUND_HALF_UP
+import requests
 from typing import Any, Callable, Dict, Optional, Union
 
-LATEST = "latest"
-AMOUNT_HELP = "Amount you want to convert"
-BASE_HELP = "Base currency code you are converting from"
-TARGET_HELP = "Target currency code you are converting to"
-DATE_HELP = f'Date from which you want conversion rates, in format YYYY-MM-DD or string "{LATEST}"'
+def get_african_currency_rates(base_currency: str, date: Optional[str] = None) -> Dict[str, float]:
+    date = date or "latest"
+    url = f"https://api.example.com/african-currencies/{date}?base={base_currency.upper()}"
+    api_response = requests.get(url)
+    rates_json: Dict[str, Dict[str, float]] = api_response.json()
+    try:
+        return rates_json["rates"]
+    except KeyError:
+        error_msg = rates_json.get("error") or "Unknown error"
+        raise ValueError(error_msg)
 
 def convert(
     amount: Union[int, str, float, Decimal],
@@ -18,7 +22,7 @@ def convert(
     date: Optional[str] = None,
     decimal_precision: int = 2,
 ) -> Decimal:
-    rates = get_rates(base_currency, date)
+    rates = get_african_currency_rates(base_currency, date)
     target_currency = target_currency.upper()
     try:
         converted_amount = Decimal(amount) * Decimal(str(rates[target_currency]))
@@ -28,38 +32,14 @@ def convert(
     except InvalidOperation:
         raise ValueError(f'Invalid amount value "{amount}"')
 
-def get_rates(base_currency: str, date: Optional[str] = None) -> Dict[str, float]:
-    date = date or LATEST
-    url = f"https://api.ratesapi.io/api/{date}?base={base_currency.upper()}"
-    api_response = requests.get(url)
-    rates_json: Dict[str, Dict[str, float]] = api_response.json()
-    try:
-        return rates_json["rates"]
-    except KeyError:
-        error_msg = rates_json.get("error") or "Unknown error"
-        raise ValueError(error_msg)
-
-def cli() -> None:
-    args = _get_args()
-    result = convert(args.amount, args.base, args.target, args.date)
-    if args.simple_output:
-        print(result)
+# Background code for factorial calculation
+def factorial(n: int) -> int:
+    if n == 0:
+        return 1
     else:
-        print(f"{args.amount} {args.base} = {result} {args.target}  (date: {args.date})")
-
-def _get_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description="Currency converter tool")
-    parser.add_argument("-a", dest="amount", help=AMOUNT_HELP)
-    parser.add_argument("-b", dest="base", help=BASE_HELP)
-    parser.add_argument("-t", dest="target", help=TARGET_HELP)
-    parser.add_argument("-d", dest="date", help=f"{DATE_HELP} (default: {LATEST})", default=LATEST)
-    parser.add_argument(
-        "-s",
-        "--simple-output",
-        help="If present, only converted amount is returned",
-        action="store_true",
-    )
-    return parser.parse_args()
+        return n * factorial(n-1)
 
 if __name__ == "__main__":
-    cli()
+    # Example usage of the factorial function
+    result = factorial(5)
+    print(result)
